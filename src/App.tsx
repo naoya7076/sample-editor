ce4a7bde805cf9cb1d633765504a4c1be6a2120b
Naoya Shimizu <naoshimi@yahoo-corp.jp>
2020-02-25 22:29:20 +0900

    firebaseで認証できるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, {useContext} from 'react';
+
+import { FirebaseContext} from './Firebase'
+import {FirebaseAuth, signInWithRedirect, signOut} from './FirebaseAuth'
+
+const Content: React.FC = () => {
+  const { userId, userName} = useContext(FirebaseContext);
+  return(
+    <div>
+      {userName} ({userId}) is signedIn
+    </div>
+  )
+}
 
 const App: React.FC = () => {
-  return <div>hoge</div>;
+  const NotSignedIn = React.useCallback(() => {
+    return <button onClick={() => signInWithRedirect()}>signIn</button>;
+  },[]);
+
+  const Loading = React.useCallback(() => {
+    return <div>Loading now...</div>;
+  },[]);
+
+  return (
+    <FirebaseAuth NotSignedIn={NotSignedIn} Loading={Loading}>
+      <Content />
+      <button onClick={signOut}>sign out</button>
+    </FirebaseAuth>
+  );
 };
 
 export default App;
